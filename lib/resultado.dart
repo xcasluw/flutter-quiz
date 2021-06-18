2a2995c0e7b27a176c64ae909c488a908b0d545a
xcasl <oligena@hotmail.com>
2021-06-18 09:23:57 -0300

    State Config - Working

---
+++ lib/resultado.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return 
+      Center(
+        child: Text(
+          'Parabéns!', 
+          style: TextStyle(fontSize: 28)
+        )
+    );
+  }
+}
